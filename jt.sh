75f1e927a18abd7a6a589d1efd69d26b15827062
gvmoorthy <gvmoorthy@gmail.com>
2023-07-31 11:53:54 +0530

    ch

---
+++ jt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo apt update
+sudo apt install openjdk-11-jre
+sudo usermod -aG sudo ubuntu
+
+sudo usermod -aG docker ubuntu
+
